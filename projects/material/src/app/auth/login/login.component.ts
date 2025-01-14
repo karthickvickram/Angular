1e5a752eaf2da3130ef4bcc00a606e1b7130a5a9
Karthick Vickram <karthick.sivanandam@exterro.com>
2025-01-14 13:41:10 +0530

    Material Login Page Reactive Form

--- projects/material/src/app/auth/login/login.component.ts
+++ projects/material/src/app/auth/login/login.component.ts
@@ -1,12 +1,70 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges, OnDestroy, OnInit, signal, SimpleChanges } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MaterialModule } from '../../material.module';
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
+import { merge, Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, MaterialModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
+  providers: [
+    { provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: {appearance: 'outline'}}
+  ]
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit, OnChanges, OnDestroy {
+
+  public loginForm!: FormGroup;
+  private destroyed$:Subject<number> = new Subject<number>();
+  public invalidEmailMsg = signal('');
+
+  constructor() {
+    
+  }
+
+  ngOnInit(): void {
+    this.loginForm = new FormGroup({
+      email: new FormControl('', {validators: [Validators.required, Validators.email]}),
+      password: new FormControl('', {validators: [Validators.required]})
+    });
+    
+    const emailControl = this.loginForm.get('email');
+    if (emailControl) {
+      merge(emailControl.valueChanges, emailControl.statusChanges)
+        .pipe(takeUntil(this.destroyed$))
+        .subscribe(() => this.updateInvalidEmail())
+    }
+    
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    
+  }
+
+  updateInvalidEmail(): void {
+    const emailFC = this.loginForm.get('email');
+    if (emailFC?.invalid) {
+      if (emailFC.hasError('required')) {
+        this.invalidEmailMsg.set('Please enter your mail');
+      } else if(emailFC.hasError('email')) {
+        this.invalidEmailMsg.set('Please enter a valid mail');
+      } else {
+        this.invalidEmailMsg.set('Please enter a valid mail');
+      }
+    } else {
+      this.invalidEmailMsg.set('');
+    }
+  }
+
+  onSubmit() {
+    console.log(this.loginForm)
+  }
+
+  ngOnDestroy(): void {
+    this.destroyed$.next(1);
+    this.destroyed$.complete();
+  }
 
 }
