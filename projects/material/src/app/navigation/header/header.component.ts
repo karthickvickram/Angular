b3e70a7828cc80a5c1e879a13ae2103b17e64d83
Karthick Vickram <karthick.sivanandam@exterro.com>
2025-01-22 22:32:00 +0530

    MatTabsModule

--- projects/material/src/app/navigation/header/header.component.ts
+++ projects/material/src/app/navigation/header/header.component.ts
@@ -1,12 +1,18 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { MaterialModule } from '../../material.module';
+import { RouterLink } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
   standalone: true,
-  imports: [MaterialModule]
+  imports: [
+    MaterialModule,
+    TranslateModule,
+    RouterLink
+  ]
 })
 export class HeaderComponent implements OnInit {
 
--- projects/material/src/app/training/training.component.ts
+++ projects/material/src/app/training/training.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import { MaterialModule } from '../material.module';
+import { NewTrainingComponent } from './new-training/new-training.component';
+import { PastTrainingComponent } from './past-training/past-training.component';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-training',
   standalone: true,
-  imports: [],
+  imports: [
+    MaterialModule,
+    TranslateModule,
+    NewTrainingComponent,
+    PastTrainingComponent
+  ],
   templateUrl: './training.component.html',
   styleUrl: './training.component.scss'
 })
