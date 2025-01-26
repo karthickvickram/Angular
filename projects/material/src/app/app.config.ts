e78af74d825ab214ab96f0793502c3c6abf17f07
Karthick Vickram <karthick.sivanandam@exterro.com>
2025-01-26 18:06:47 +0530

    Adding Firebase in Angular

--- projects/material/src/app/app.config.ts
+++ projects/material/src/app/app.config.ts
@@ -13,12 +13,28 @@ export function translateLoaderFactory(http: HttpClient) {
   return new TranslateHttpLoader(http, 'assets/i18n/', '.json')
 }
 
+//FireBase
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDrxJ9ow_fhtOHP9i1P1oXX8ArZR7Cw6XQ",
+  authDomain: "ng-fitness-tracker-ff01d.firebaseapp.com",
+  projectId: "ng-fitness-tracker-ff01d",
+  storageBucket: "ng-fitness-tracker-ff01d.firebasestorage.app",
+  messagingSenderId: "163204165570",
+  appId: "1:163204165570:web:1b1752f58210dfd6df305b",
+  measurementId: "G-V14FE9DE85"
+};
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes), 
     provideAnimationsAsync(),
     provideHttpClient(withFetch()),
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideFirestore(() => getFirestore()),
     importProvidersFrom([
       TranslateModule.forRoot({
         defaultLanguage: 'en',
