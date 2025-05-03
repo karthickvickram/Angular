bbd3558fb96859bba263ae4ee97561ded36eeace
Karthick Vickram <karthick.sivanandam@exterro.com>
2025-05-03 21:36:03 +0530

    App Route

--- projects/fuel-tracker/src/app/app.routes.ts
+++ projects/fuel-tracker/src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'entry',
+        loadComponent: () => import('./fuel-entry/fuel-entry.component').then(m => m.FuelEntryComponent)
+    },
+    { 
+        path: 'log', 
+        loadComponent: () => import('./fuel-list/fuel-list.component').then(m => m.FuelListComponent) 
+    },
+    { 
+        path: 'chart', 
+        loadComponent: () => import('./fuel-dashboard/fuel-dashboard.component').then(m => m.FuelDashboardComponent) 
+    },
+    { 
+        path: 'metrics', loadComponent: () => import('./fuel-dashboard/fuel-dashboard.component').then(m => m.FuelDashboardComponent) 
+    }
+];
