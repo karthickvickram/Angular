b9eefa7e621d817ae8775258342c7c8b3c6deb75
Karthick Vickram <karthick.sivanandam@exterro.com>
2025-05-23 08:34:49 +0530

    Fuel MEtrics

--- projects/fuel-tracker/src/app/fuel-metrics/fuel-metrics.component.ts
+++ projects/fuel-tracker/src/app/fuel-metrics/fuel-metrics.component.ts
@@ -4,25 +4,32 @@ import { Subject, takeUntil } from 'rxjs';
 import { FuelEntry } from '../Model/Fuel';
 import { CommonModule } from '@angular/common';
 import { CardModule } from 'primeng/card';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-fuel-metrics',
   templateUrl: './fuel-metrics.component.html',
   styleUrls: ['./fuel-metrics.component.scss'],
-  imports: [CommonModule, CardModule]
+  imports: [CommonModule, CardModule, MatIconModule],
 })
 export class FuelMetricsComponent implements OnInit, OnDestroy {
   
-  fuelEntries: FuelEntry[] = [];
+  private fuelEntries: FuelEntry[] = [];
   private destroyed$ = new Subject<void>();
 
-  totalDistance = 0;
-  totalFuel = 0;
-  totalCost = 0;
-  mileage = 0;
-  fuelEconomy = 0;
+  public totalEntries = 0;
+  public totalDistance = 0;
+  public totalFuel = 0;
+  public totalCost = 0;
+  public mileage = 0;
+  public fuelEconomy = 0;
+
+  public filterMonth: Date = new Date();
+  public monthAmt: number = 0;
+  public filterYear: Date = new Date();
+  public yearAmt: number = 0;
   
-  constructor(private fuelService: FuelService) { }
+  constructor(private fuelService: FuelService) {}
 
   ngOnInit() {
     this.fuelService.fuelEntries$
@@ -33,6 +40,8 @@ export class FuelMetricsComponent implements OnInit, OnDestroy {
       next: (entries) => {
         this.fuelEntries = entries;
         this.getMetrics();
+        this.getAmtByMonth();
+        this.getAmtByYear();
       },
       error: (err) => {
         this.fuelEntries = [];
@@ -42,6 +51,7 @@ export class FuelMetricsComponent implements OnInit, OnDestroy {
   }
 
   getMetrics() {
+    this.totalEntries = this.fuelEntries.length;
     this.totalFuel = this.fuelEntries.reduce((sum, e) => sum + e.liter, 0);
     this.totalCost = this.fuelEntries.reduce((sum, e) => sum + e.amount, 0);
 
@@ -60,6 +70,40 @@ export class FuelMetricsComponent implements OnInit, OnDestroy {
       ? this.totalCost / this.totalDistance
       : 0;
 
+  }
+
+  onMonthNavigation(dir: 'prev' | 'next') {
+    const newDate = new Date(this.filterMonth);
+    newDate.setMonth(newDate.getMonth() + (dir == 'prev' ? -1 : 1));
+    this.filterMonth = newDate;
+    this.getAmtByMonth();
+  }
+
+  onYearNavigation(dir: 'prev' | 'next') {
+    const newDate = new Date(this.filterYear);
+    newDate.setFullYear(newDate.getFullYear() + (dir == 'prev' ? -1 : 1));
+    this.filterYear = newDate;
+    this.getAmtByYear();
+  }
+
+  getAmtByMonth() {
+    this.monthAmt = this.fuelEntries.reduce((sum, entry) => {
+      let date = new Date(entry.date);
+      if (date.getMonth() == this.filterMonth.getMonth() && date.getFullYear() == this.filterMonth.getFullYear()) {
+        sum += entry.amount;
+      }
+      return sum;
+    }, 0)
+  }
+
+  getAmtByYear() {
+    this.yearAmt = this.fuelEntries.reduce((sum, entry) => {
+      let date = new Date(entry.date);
+      if (date.getFullYear() == this.filterYear.getFullYear()) {
+        sum += entry.amount;
+      }
+      return sum;
+    }, 0)
   }
 
   ngOnDestroy(): void {
